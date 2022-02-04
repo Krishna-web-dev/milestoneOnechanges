56fa09fac7b6db395c320dc5231ef1ddc32fa666
Krishna-web-dev <69578860+Krishna-web-dev@users.noreply.github.com>
2022-02-04 11:57:04 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,10 @@
+package com.firstProject;
+
+public class calculator {
+    public int doSum( int a , int b){
+        return a+b;}
+
+    public int multiply( int a , int b){
+
+        return a*b;}
+}
